3bb42bb25e7dab56db0b34cfaab232053165ce4a
sinanaserig <120180094+sinanaserig@users.noreply.github.com>
2022-12-09 14:52:08 +0330

    inistyles register

---
+++ naseri.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+  <meta charset="UTF-8">
+  <title>ثبت نام در سایت</title>
+<html>
+<body>
+
+  <div id="users">
+    <form action="" method=>
+        <input type="text" name="display-name" class="input" placeholder="نام شما...."><br>
+        <input type="text" name="email" class="input" placeholder="ایمیل شما..."><br>
+        <input type="password" name="password" class="input" placeholder="رمز شما..."><br>
+        <input type="password" name="password-conf" class="input" placeholder=" تایید رمز..."><br>
+        <input type="submit" name="do_register" value="ثبت نام">
+    </form>
+  </div>
+
+</body>
+</html>
